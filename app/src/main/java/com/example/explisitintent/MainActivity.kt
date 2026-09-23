3083db18c0563ad88fbab7ee95d609b3fb3aabd9
Evan Suryadinata <evanxvanguard@gmail.com>
2026-09-23 19:54:54 +0700

    Feat : Switch to Another Main, with selected Data

--- app/src/main/java/com/example/explisitintent/MainActivity.kt
+++ app/src/main/java/com/example/explisitintent/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.explisitintent
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,8 +22,23 @@ class MainActivity : AppCompatActivity() {
 
         var _btnExplisit1 = findViewById<Button>(R.id.btnExplisit1)
         _btnExplisit1.setOnClickListener {
-            val intent = Intent(this@MainActivity, MainActivity2::class.java)
+            val intent = Intent(
+                this@MainActivity,
+                MainActivity2::class.java)
             startActivity(intent)
+        }
+
+        val _dataKirim = findViewById<EditText>(R.id.dataKirim)
+        val _btnExplisit2 = findViewById<Button>(R.id.btnExplisit2)
+
+        _btnExplisit2.setOnClickListener {
+            val intentWithData = Intent(
+                this@MainActivity,
+                MainActivity3::class.java)
+                .apply {
+                    putExtra(MainActivity3.dataTerima, _dataKirim.text.toString())
+                }
+            startActivity(intentWithData)
         }
     }
 }
---
+++ app/src/main/java/com/example/explisitintent/MainActivity3.kt
@@ -0,0 +1,29 @@
+package com.example.explisitintent
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main3)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val data = intent.getStringExtra(dataTerima)
+        val _showData = findViewById<TextView>(R.id.showData)
+        _showData.text = data?.toString()
+    }
+
+    companion object{
+        const val dataTerima = "extra_dataTerima"
+    }
+}
